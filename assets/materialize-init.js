389b6c598e4526138255829985575670fa6a3470
Rajesh Sivasankaran <rajesh.sivasankaran@zeiss.com>
2019-08-10 11:01:46 +0530

    geo update

--- assets/materialize-init.js
+++ assets/materialize-init.js
@@ -1,16 +1,23 @@
 function initMatComp() {
     initMatModal();
     initMatSlide();
-  }
-  function initMatModal() {
+    initMatCollapsible();
+}
+function initMatModal() {
     document.addEventListener('DOMContentLoaded', function () {
-      var elems = document.querySelectorAll('.modal');
-      M.Modal.init(elems);
+        var elems = document.querySelectorAll('.modal');
+        M.Modal.init(elems);
     });
-  }
-  function initMatSlide() {
+}
+function initMatSlide() {
     document.addEventListener('DOMContentLoaded', function () {
-      var elems = document.querySelectorAll('.sidenav');
-      M.Sidenav.init(elems);
+        var elems = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(elems);
     });
-  }
+}
+function initMatCollapsible() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.collapsible');
+        M.Collapsible.init(elems);
+      });
+}
