c464b95287083481d85bf97ea3abbe2765656b72
Rajesh Sivasankaran <rajesh.sivasankaran@zeiss.com>
2020-05-02 21:26:34 +0530

    Apply app with events

--- ApplyApp/src/app/app.component.ts
+++ ApplyApp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Time } from '@angular/common';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,40 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  event = this.fb.group({
+    title: [''],
+    from: [new Date()],
+    to: [new Date()],
+    address: [''],
+    locationUrl: ['']
+  });
+  events = this.fb.array([
+    this.event
+  ]);
   applyForm = this.fb.group({
-    groomBrideName: [''],
+    groomBrideName: ['Raj'],
     brideGroomName: [''],
     weddingDate: [new Date()],
-    weddingTime: ['']
+    weddingTime: [new Date()],
+    events: this.events
   });
   constructor(private fb: FormBuilder) { }
+
+  applySubmit() {
+    console.log(this.applyForm);
+
+
+  }
+
+  get eventsArray() {
+    return this.applyForm.get('events') as FormArray;
+  }
+
+  addEvent() {
+    this.eventsArray.push(this.event);
+  }
+
+  remveEvent(i: number) {
+    this.eventsArray.removeAt(i);
+  }
 }
