7ecfb6798ba1a657d625d39fcf0c25a720cf81f3
Rajesh Sivasankaran <rajesh.sivasankaran@zeiss.com>
2020-07-11 10:36:59 +0530

    fix

--- wed/service-worker.js
+++ wed/service-worker.js
@@ -13,9 +13,9 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/precache-manifest.3dfb6cccf07a09c27d3963284e8cba88.js"
-);
+// importScripts(
+//   "/precache-manifest.3dfb6cccf07a09c27d3963284e8cba88.js"
+// );
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
